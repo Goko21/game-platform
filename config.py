5925057805bcd695696c3ffe7dd0198297104451
Goko21 <gorkemerrr_55@hotmail.com>
2025-05-05 20:47:40 +0300

    First config

---
+++ config.py
@@ -0,0 +1,5 @@
+# config.py
+from pymongo import MongoClient
+
+# MongoDB bağlantısı
+client = MongoClient('mongodb+srv://gorkemerrr55:rSli7CuYAF9h0Dx2@gameplatform.ik3xjkw.mongodb.net/?retryWrites=true&w=majority&appName=GamePlatform')
