3275d60f7c0985a5be4ca9c05acfbab814e30b4c
Goko21 <gorkemerrr_55@hotmail.com>
2025-05-08 20:35:44 +0300

    Fix game name formatting in game detail route

--- app.py
+++ app.py
@@ -161,7 +161,7 @@ def games():
 @app.route('/game/<game_name>')
 def game_detail(game_name):
     print(game_name)
-    if "%20%" in game:
+    if "%20" in game:
           game_name = game_name.replace("%20", " ")
     game = games_col.find_one({"name": game_name})
     if not game:
